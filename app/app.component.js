93c4c89acb66faf54ef8c8fc63567bf6cbbaec89
Ben S <allobon@gmail.com>
2016-09-20 21:36:38 +0100

    firsy

---
+++ app/app.component.js
@@ -0,0 +1,31 @@
+"use strict";
+var Hero = (function () {
+    function Hero() {
+    }
+    return Hero;
+}());
+exports.Hero = Hero;
+var hays = [
+    { id: 11, name: 'Mr. Nice' },
+    { id: 12, name: 'Narco' },
+    { id: 13, name: 'Bombasto' },
+    { id: 14, name: 'Celeritas' },
+    { id: 15, name: 'Magneta' },
+    { id: 16, name: 'RubberMan' },
+    { id: 17, name: 'Dynama' },
+    { id: 18, name: 'Dr IQ' },
+    { id: 19, name: 'Magma' },
+    { id: 20, name: 'Tornado' }
+];
+var AppComponent = (function () {
+    function AppComponent() {
+        this.title = "tour of heroes";
+        this.hero = {
+            id: 1,
+            name: "Twatface"
+        };
+    }
+    return AppComponent;
+}());
+exports.AppComponent = AppComponent;
+//# sourceMappingURL=app.component.js.map
